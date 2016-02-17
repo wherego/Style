c5f68c85d127e6bd95a49144ae9154581bc3f6e8
boxuanjia <boxuanjia@gmail.com>
2016-02-17 13:16:16 +0800

    update

--- app/src/main/java/com/boxuanjia/style/ui/BaseActivity.java
+++ app/src/main/java/com/boxuanjia/style/ui/BaseActivity.java
@@ -1,8 +1,51 @@
 package com.boxuanjia.style.ui;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+
+import com.orhanobut.logger.Logger;
 
 public abstract class BaseActivity extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Logger.d("onCreate");
+    }
 
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Logger.d("onRestart");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Logger.d("onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Logger.d("onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Logger.d("onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Logger.d("onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Logger.d("onDestroy");
+    }
 }
